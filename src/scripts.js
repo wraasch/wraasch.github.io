59ed1fb257aeeefe834cb4e07904f4b916345ec7
Will Raasch <will@readyset.film>
2021-05-02 21:31:49 -0700

    Adding cookies

--- src/scripts.js
+++ src/scripts.js
@@ -29,4 +29,43 @@ $('#diabetes-algorithm').click(function(){
     } else {
         $('.diabetes-no').show();
     }
+});
+
+// Profile Cookies
+$("#profile-submit").click(function() {
+    // Create dictionary for profile info
+    var profileInfo = {};
+    // Set key-value pairs for each profile attribute
+    profileInfo["name"] = $("#name").val();
+    profileInfo["profilePic"] = $("#pic").val();
+    profileInfo["role"] = $("#role").val();
+    // Create empty diseases list
+    var diseases = [];
+    // Find checked checkboxes
+    $("input[type='checkbox']:checked").each(function() {
+      // Get ID of checked button and add to diseases list
+      diseases.push($(this).attr("id"));
+    });
+    profileInfo["diseases"] = diseases;
+    // Save profile dictionary as a cookie
+    Cookies.set("profile", profileInfo);
   });
+  // If profile cookie exists, show profile page
+  if (Cookies.get("profile")) {
+    var profileDataFromCookie = Cookies.get("profile");
+    // You need to parse dictionaries
+    var profileInfo = JSON.parse(profileDataFromCookie);
+    // Add profile info into HTML
+    $("#insert-profile-name").text(profileInfo["name"]);
+    $("#insert-profile-role").text(profileInfo["role"]);
+    $("#insert-profile-pic").attr("src", profileInfo["profilePic"]);
+    var diseaseHTML = "";
+    // Iterate through diseases list and add to html
+    profileInfo["diseases"].forEach(function(item) {
+      diseaseHTML += "<p class='disease-text'>" + item + "</p>";
+    });
+    $("#insert-profile-disease").html(diseaseHTML);
+    // Show profile HTML, and hide form
+    $(".personal-profile").show();
+    $(".anonymous-profile").hide();
+  }
