4593c884c090b26b589a5f73b4f69a77123c3e74
Will Raasch <will@readyset.film>
2022-04-25 16:28:20 -0700

    Commits from ALS414 lecture 6

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,16 @@
 // Add scripts here
+// When the success-alert id (hashtag) is clicked
+$("#success-alert").click(function() {
+    // Do something
+    alert("Hello! I am an alert box!!");
+});
+
+// Listener for the class (period) for click-here-to-show
+$(".click-here-to-toggle").click(function() {
+    // Do something
+    if (confirm("Are you sure you want to toggle the menu items?") == true) {
+        $("#menu-items").show();
+      } else {
+        alert("Oops!");
+      }
+});
