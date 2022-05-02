afdf335c4b9041ea686d1c21948c50a72d042681
Will Raasch <will@readyset.film>
2022-05-02 15:25:40 -0700

    Adding data tables and cookies

--- src/scripts.js
+++ src/scripts.js
@@ -38,3 +38,25 @@ $("#calc-diabetes").click(function() {
     }
     
 });
+
+// JAVASCRIPT DATABASE TO TABLES.HTML TABLE
+// Creating array of objects
+var information = [{'name': 'Jane Doe', 'phone': '555-444-3333', 'position': 'Patient', 'rank': 5},
+                    {'name': 'John Doe', 'phone': '555-667-7474', 'position': 'Provider', 'rank': 3},
+                    {'name': 'Jose Doe', 'phone': '555-223-3232', 'position': 'Payer', 'rank': 1},
+                    {'name': 'Juan Doe', 'phone': '555-989-1212', 'position': 'Producer', 'rank': 2},
+                    {'name': 'Will Raasch', 'phone': '202-253-7251', 'position': 'Teacher', 'rank': 6}
+                    ];
+
+// Sorting array by rank order
+information.sort((a,b) => b.rank - a.rank);
+
+// Filling in data table rows
+information.forEach(function (item, index) {
+  $('#data-table tr:last').after('<tr><td>' + item['name'] + '</td><td>' + item['phone'] + '</td><td>' + item['position'] + '</td><td>' + item['rank'] + '</td></tr>');
+});
+
+// COOKIES
+Cookies.set('user', 'Will');
+var username = Cookies.get('user');
+$('#welcome-text').text(username);
